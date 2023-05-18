ad1469a81c5375f8e1db5fd60550db97ae943fed
Vlad <114685838+VladislavFisun@users.noreply.github.com>
2023-05-18 22:38:16 +0300

    fonts

---
+++ src/components/LoadingItem/LoadingItem.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoadingItem = () => {
+    return (
+      
+            <img style={{height:'50%'}} src="https://media.tenor.com/rtY9m7EokSYAAAAC/cat-loading.gif" alt="" />
+        
+    );
+};
+
+export default LoadingItem;
--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -3,11 +3,13 @@ import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
 import {useEffect,useState} from 'react'
 import { v4 as uuidv4 } from 'uuid';
+import TableCell from '@mui/material/TableCell';
 import { useAppDispatch,useAppSelector } from '../../hooks';
 import { getData, updateCurrentPage } from '../../redux/MainSlice';
 import { DragDropContext, Draggable, Droppable, DropResult } from "react-beautiful-dnd"
-import { currencyType } from '../../types';
-
+import { getRightDate } from '../../utils';
+import { TableBody, TableContainer, TableRow,TableHead,Table} from '@mui/material';
+import LoadingItem from '../LoadingItem/LoadingItem';
 
 const Main = () => {
     const newData = useAppSelector(state=>state.main)
@@ -17,19 +19,19 @@ const Main = () => {
       return { ...item,id:uuidv4()}
       }))
 
-
+console.log(thisPage)
     useEffect(()=>{
       dispatch(getData('https://api.iex.cloud/v1/data/CORE/HISTORICAL_PRICES/aapl,fb,googl,amzn,msft,nflx,twtr,tsla,brk.b,jpm,ba,wmt,ma,v,pg,dis,c,hd,unh,ko,pep,pfe,axp,nke,mcd,mmm,mrk,ba,cvx,cat,ge,jnj,ibm,orcl,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba,ba?token=sk_edd20de2d9be43fcbef97407d7e9b2ba'))
 
     },[])
     useEffect(()=>{
-     dispatch(updateCurrentPage(thisPage))
+  
     },[newData.data,page])
 useEffect(()=>{
   setThisPage(newData.data.slice(page,10+page).map(item=>{
     return { ...item,id:uuidv4()}
     }))
-},[page])
+},[page,newData.data])
  
 
    const onDragHandle = (result: DropResult) => {
@@ -49,32 +51,63 @@ useEffect(()=>{
     return (
         <div className='Main_content'>
             <Paper elevation={3} className='Main_container'>
-            <DragDropContext onDragEnd={onDragHandle}>
-				<Droppable droppableId="todo" type='list'>
-					{(provided) => (
-						<div className="currency" {...provided.droppableProps} ref={provided.innerRef}>
-							{thisPage.map((item, index) => {
-								return (
-									<Draggable key={item.id} draggableId={item.id} index={index}>
-										{(provided) => (
-											<div
-                      
-
-												ref={provided.innerRef}
-												{...provided.draggableProps}
-												{...provided.dragHandleProps}
-												
-											>
-                      {item.close}
-											</div>
-										)}
-									</Draggable>
-								)
-							})}
-						</div>
-					)}
-				</Droppable>
-			</DragDropContext>
+              {newData.status==='loading'?<LoadingItem/>
+              : <TableContainer>
+              <Table sx={{ minWidth: 650 }}  aria-label="simple table">
+              <TableHead>
+                      <TableRow>
+                        <TableCell>Currency symbol</TableCell>
+                        <TableCell align="right">close</TableCell>
+                        <TableCell align="right">high</TableCell>
+                        <TableCell align="right">flow</TableCell>
+                        <TableCell align="right">fvolume</TableCell>
+                        <TableCell align="right">fclose</TableCell>
+                        <TableCell align="right">fhigh</TableCell>
+                        <TableCell align="right">date</TableCell>
+                      </TableRow>
+                    </TableHead>
+                
+                              <DragDropContext onDragEnd={onDragHandle}>
+                          <Droppable droppableId="currency" type='list'>
+                            {(provided) => (
+                              <TableBody style={{width:'770px'}} {...provided.droppableProps} ref={provided.innerRef}>
+                                {thisPage.map((item, index) => {
+                                  return (
+                                    <Draggable key={item.id} draggableId={item.id} index={index}>
+                                      {(provided) => (
+                                      <TableRow
+                                      key={item.id}
+                                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                      ref={provided.innerRef}
+                                      {...provided.draggableProps}
+                                      {...provided.dragHandleProps}>
+                                        
+                                    
+                                      <TableCell align="left">{item.symbol}</TableCell>
+                                      <TableCell align="left">{item.close}</TableCell>
+                                      <TableCell align="left">{item.high}</TableCell>
+                                      <TableCell align="left">{item.flow}</TableCell>
+                                      <TableCell align="left">{item.fvolume}</TableCell>
+                                      <TableCell align="left">{item.fclose}</TableCell>
+                                      <TableCell align="left">{item.fhigh}</TableCell>
+                                      <TableCell align="left">{getRightDate(item.date)}</TableCell>
+                                        
+                                         
+                                      </TableRow>
+                                      )}
+                                    </Draggable>
+                                  )
+                                })}
+                                {provided.placeholder}
+                              </TableBody>
+                            )}
+                          </Droppable>
+                        </DragDropContext>
+            
+              </Table>
+               </TableContainer>
+              }
+  
 
 <div className='Main_buttons'>
         <Button 
